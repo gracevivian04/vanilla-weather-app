bbd907b2e08f0aca9bf38d8c7e20534413445b85
gracevivian04 <100720110+gracevivian04@users.noreply.github.com>
2022-04-27 19:06:41 +0100

    Added relevant icon from API

--- src/app.js
+++ src/app.js
@@ -9,31 +9,31 @@ function formatDate(timestamp) {
         minutes = `0${minutes}`;
     }
     let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    let day = date.getDay();
+    let day = days[date.getDay()];
     return `${day} ${hours}:${minutes}`;
 }
 
-
-
-
 function displayTemperature(response) {
-    console.log(response.data);
     let temperatureElement = document.querySelector("temperature");
     let cityElement = document.querySelector("#city");
     let descriptionElement = document.querySelector("#description");
     let humidityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#wind");
     let dateElement = document.querySelector("#date");
+    let iconElement = document.querySelector("#icon");
+    
     temperatureElement.innerHTML = Math.round (response.data.main.temp);
     cityElement.innerHTML = response.data.name;
     descriptionElement.innerHTML = response.data.weather[0].description;
     humidityElement.innerHTML = response.data.main.humidity;
     windElement.innerHTML = Math.round(response.data.wind.speed);
     dateElement.innerHTML = formatDate(response.data.dt*1000); 
+    iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+    iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 let apiKey = "cdec7dc86f02673d413e36697779fb47";
+let city = "";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Barcelona&appid={cdec7dc86f02673d413e36697779fb47}&units=metric`;
 
-console.log(apiUrl);
 axios.get(apiUrl).then(displayTemperature);
