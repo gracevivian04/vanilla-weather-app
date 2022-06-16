a4af7b850e031a4cf92ca97d12a0134e56112071
gracevivian04 <100720110+gracevivian04@users.noreply.github.com>
2022-06-16 17:35:39 +0100

    removed console log

--- src/app.js
+++ src/app.js
@@ -45,14 +45,13 @@ function displayForecast(response) {
              </div>
     `;
         }
-    )};
+    });
 
     forecastHTML = forecastHTML + `</div>`;
     forecastElement.innerHTML = forecastHTML;
 }
 
 function getForecast(coordinates){
-  console.log(coordinates);
   let apiKey = "cdec7dc86f02673d413e36697779fb47";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&unit=metric`;
   axios.get(apiUrl).then(displayForecast);
